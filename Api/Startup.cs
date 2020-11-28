98faeed7c557aebe8017289fda942ed8fcbc07e0
weberticf@gmail.com <weberticf@gmail.com>
2020-11-27 23:20:45 -0300

    add pacote mogodb driver

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -31,16 +31,9 @@ namespace Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
